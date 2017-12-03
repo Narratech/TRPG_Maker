c4dcbdccc88bd099eda728b8c3598ce0cdfc713b
Marcelino Pérez <32309834+mperez01@users.noreply.github.com>
2017-12-03 15:02:36 +0100

    Items check
    Now, if try to add a item in a slot without slot type select, don't crashed. Added a new dialog showing why can't add an item

--- Assets/RPGMaker/Scripts/Character.cs
+++ Assets/RPGMaker/Scripts/Character.cs
@@ -37,11 +37,13 @@ public class Character: MonoBehaviour{
             EditorGUILayout.PropertyField(tps, true);
             if (EditorGUI.EndChangeCheck())
                 serializedObject.ApplyModifiedProperties();
+			Boolean errorType = false;
 
             // Check if item correspond with slot
             for (int i = 0; i < myScript.Slots.Count; i++) {
                 Boolean correct = true;
                 // Si el slot actual tiene item
+			
                 if (myScript.Slots[i].item != null)
                 {
                     // Si el item tiene este tipo de slot
@@ -50,35 +52,41 @@ public class Character: MonoBehaviour{
                             {
                                 return slotType == myScript.Slots[i].slotType;
                             });
-                    // Si no lo contiene, error
-                    if (posAct == -1)
-                        correct = false;
+					// Si no lo contiene, error 
+					if (posAct == -1) {
+						EditorUtility.DisplayDialog("Error",
+							"The item " + myScript.Slots [i].item.name + 
+							" does not correspond with the slot type " + myScript.Slots[i].slotType , "Accept");
+						errorType = true;
+						correct = false;
+					}
                     
                     //Para cada tipo de slot del item
-                    for(int j = 0; j < myScript.Slots[i].item.SlotType.Count; j++)
-                    {
+					for (int j = 0; j < myScript.Slots [i].item.SlotType.Count; j++) {
+						//si esta definido el tipo de slot
+						if (myScript.Slots [i].slotType != null) {
                         // Si el slot es distinto del actual
-                        if(myScript.Slots[i].item.SlotType[j].GetInstanceID() != myScript.Slots[i].slotType.GetInstanceID())
-                        {
-                            // Buscamos el slot correspondiente
-                            int pos = myScript.Slots.FindIndex(
-                            delegate (Slot slot)
-                            {
-                                return slot.slotType == myScript.Slots[i].item.SlotType[j];
-                            });
+                        if (myScript.Slots [i].item.SlotType [j].GetInstanceID () != myScript.Slots [i].slotType.GetInstanceID ()) {
+							// Buscamos el slot correspondiente
+							int pos = myScript.Slots.FindIndex (
+									delegate (Slot slot) {
+									return slot.slotType == myScript.Slots [i].item.SlotType [j];
+								});
                             
-                            // Si tiene un item, error
-                            if (myScript.Slots[pos].item != null)
-                            {
-                                correct = false;                             
-                            }
-                        } 
+							// Si tiene un item, error
+							if (myScript.Slots [pos].item != null) {
+								correct = false;
+							}
+						} 
+					}
                     }
                 }
                 if (!correct) {
-                    EditorUtility.DisplayDialog("Error",
-                      "The item does not correspond with this slot", "Accept");
-                    myScript.Slots[i].item = null;
+					if (errorType == false) {
+	                    EditorUtility.DisplayDialog("Error",
+	                      "The item does not correspond with this slot", "Accept");
+					}
+					myScript.Slots[i].item = null;
                 }
             }
         }
